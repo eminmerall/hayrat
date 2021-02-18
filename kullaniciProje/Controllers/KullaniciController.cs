52c7bd2627a39e3a600c5bfc0fe08291ddc07949
Emin MERAL <emin.meral@ogr.sakarya.edu.tr>
2021-02-18 23:35:28 +0300

    ilk commit

---
+++ kullaniciProje/Controllers/KullaniciController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.Validation;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using kullaniciProje.Models.Entity;
+
+namespace kullaniciProje.Controllers
+{
+    public class KullaniciController : Controller
+    {
+        // GET: Kullanici
+        kullaniciProjeEntities1 db = new kullaniciProjeEntities1();
+        public ActionResult kullaniciListele()
+        {
+            var kullanicilar = db.Kullanicilar.ToList();
+            return View(kullanicilar);
+        }
+        [HttpGet]
+        public ActionResult kullaniciEkleme()
+        {
+            return View();
+        }
+        
+        [HttpPost]
+        public ActionResult kullaniciEkleme(Kullanicilar p1)
+        {
+            db.Kullanicilar.Add(p1);
+            db.SaveChanges();
+            return RedirectToAction("kullaniciListele");
+        }
+
+        public ActionResult sil(int id)
+        {
+            var kullaniciSil = db.Kullanicilar.Find(id);
+            db.Kullanicilar.Remove(kullaniciSil);
+            db.SaveChanges();
+            return RedirectToAction("kullaniciListele");
+        }
+
+        public ActionResult kullaniciGetir(int id)
+        {
+            var klncGetir = db.Kullanicilar.Find(id);
+            return View("kullaniciGetir", klncGetir);
+        }
+    }
+
+
+}
---
+++ kullaniciProje/ViewModels/UsersVM.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace kullaniciProje.ViewModels
+{
+    public class UsersVM
+    {
+        public int Id { get; set; }
+        public string K_Ad { get; set; }
+        public string Ad_Soyad { get; set; }
+        public string Sifre { get; set; }
+        public string Grup { get; set; }
+        public string K_Tur { get; set; }
+    }
+
+    public class UsersListVM
+    {
+        public List<UsersVM> users { get; set; }
+    }
+}
